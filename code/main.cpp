94560de6e148c4cf3a927673e56b3809b183f36d
Subroto <subrotoph@gmail.com>
2020-08-20 01:25:24 +0900

    Ignore mac files

--- code/main.cpp
+++ code/main.cpp
@@ -23,7 +23,7 @@ int main() {
     glm::vec4 vec;
     auto test = matrix * vec;
 
-    while (!glfwWindowShouldClose(window)) {
+    while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
 
