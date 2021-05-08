7d892f94b2fce46f0b5705d55ca7e54dcca3e558
Subroto <subrotoph@gmail.com>
2021-05-08 17:41:34 +0900

    Update resource path

--- code/process/graphic_main.h
+++ code/process/graphic_main.h
@@ -30,14 +30,14 @@ class GraphicMain {
     
 public:
     const std::string MODEL_PATH = "models/bunny/bunny.obj";
-    const uint TEX_IDX = 3; // 3,4,
+    const uint TEX_IDX = 6; // 3,4,
     const std::vector<std::string> TEXTURES = {"cliffrockface", "cobblestylized", "greasypan", "layered-rock1", "limestone6",  "roughrockface", "rustediron", "slimy-slippery-rock1", "slipperystonework", "worn-wet-old-cobblestone"};
     const std::string TEX_PATH = TEXTURES[TEX_IDX] + "/" + TEXTURES[TEX_IDX];
-    const std::string TEX_ALBEDO_PATH   = "textures/" + TEX_PATH + "_albedo.png";
-    const std::string TEX_AO_PATH       = "textures/" + TEX_PATH + "_ao.png";
-    const std::string TEX_METALLIC_PATH = "textures/" + TEX_PATH + "_metallic.png";
-    const std::string TEX_NORMAL_PATH   = "textures/" + TEX_PATH + "_normal.png";
-    const std::string TEX_ROUGNESS_PATH = "textures/" + TEX_PATH + "_roughness.png";
+    const std::string TEX_ALBEDO_PATH   = "textures/pbr/" + TEX_PATH + "_albedo.png";
+    const std::string TEX_AO_PATH       = "textures/pbr/" + TEX_PATH + "_ao.png";
+    const std::string TEX_METALLIC_PATH = "textures/pbr/" + TEX_PATH + "_metallic.png";
+    const std::string TEX_NORMAL_PATH   = "textures/pbr/" + TEX_PATH + "_normal.png";
+    const std::string TEX_ROUGNESS_PATH = "textures/pbr/" + TEX_PATH + "_roughness.png";
     const std::vector<std::string> TEXURES_PATH = {
         TEX_ALBEDO_PATH, TEX_AO_PATH, TEX_METALLIC_PATH,
         TEX_NORMAL_PATH, TEX_ROUGNESS_PATH };
